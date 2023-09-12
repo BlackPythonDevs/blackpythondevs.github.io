2f679c9c8cc77a3b35934ade7a8bc4897781fe5b
Oleksis Fraga Menéndez <oleksis.fraga@gmail.com>
2023-09-12 10:58:50 -0400

    Format and Lint using pre-commit hooks

--- assets/js/bpd.js
+++ assets/js/bpd.js
@@ -3,13 +3,16 @@
  */
 
 // Menu on small screens
-let menuToggle = document.querySelectorAll('.menu-toggle');
+let menuToggle = document.querySelectorAll(".menu-toggle");
 if (menuToggle) {
-    for (let i = 0; i < menuToggle.length; i++) {
-        menuToggle[i].addEventListener('click', function (e) {
-            document.body.classList.toggle('menu--opened');
-            e.preventDefault();
-        }, false);
-    }
+  for (let i = 0; i < menuToggle.length; i++) {
+    menuToggle[i].addEventListener(
+      "click",
+      function (e) {
+        document.body.classList.toggle("menu--opened");
+        e.preventDefault();
+      },
+      false,
+    );
+  }
 }
-;
