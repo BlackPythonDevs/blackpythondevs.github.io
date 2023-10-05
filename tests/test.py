fcb95ed87145f9c365996d372126ab7f2c8c0f90
Oleksis Fraga Menéndez <oleksis.fraga@gmail.com>
2023-10-05 15:57:21 -0400

    Update test : check the page use html language code english/spanish test_destination_options and test_destination_ look the same

--- tests/test.py
+++ tests/test.py
@@ -1,7 +1,6 @@
 import pytest
 from playwright.sync_api import Page, expect
 
-
 live_server_url = "https://blackpythondevs.github.io"
 
 routes = [
@@ -13,18 +12,7 @@ routes = [
 
 
 @pytest.mark.parametrize("url", routes)
-def test_destination_options(
-    page: Page,
-    url: str,
-):
-    """Test that the destinations page loads with seeded data"""
-    # Create a destination
-    page.goto(f"{live_server_url}/{url}")
-    page.on("response", lambda response: expect(response.status).to_equal(200))
-
-
-@pytest.mark.parametrize("url", routes)
-def test_destination_(
+def test_destination(
     page: Page,
     url: str,
 ) -> None:
@@ -46,6 +34,10 @@ def test_destination_(
 )
 def test_headers_in_language(page: Page, title: str, url: str) -> None:
     page.goto("https://blackpythondevs.github.io/")
+    lang = page.evaluate("document.documentElement.lang")
+    assert lang == "en"
     page.get_by_label("Language").select_option("es")
     page.get_by_role("link", name=title).click()
     expect(page).to_have_url(f"https://blackpythondevs.github.io{url}")
+    lang = page.evaluate("document.documentElement.lang")
+    assert lang == "es"
