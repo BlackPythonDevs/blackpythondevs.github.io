7adcc43db799a9afe58565442b7c869c65f1045c
Oleksis Fraga Menéndez <oleksis.fraga@gmail.com>
2023-10-16 13:09:09 -0400

    [js] Optionally support ISO 3166-1 alpha-2

--- assets/js/bpd.js
+++ assets/js/bpd.js
@@ -20,7 +20,8 @@ if (menuToggle) {
 function loadLanguage(lang) {
   let base_pathname = window.location.pathname;
   // ISO 639-1 language codes are two-letter codes
-  let lang_path_regex = /^\/[a-z]{2}([_-][a-z]+)?\//;
+  // ISO 3166-1 alpha-2 country codes are two-letter codes
+  let lang_path_regex = /^\/[a-z]{2}([_-][A-Z]{2})?\//;
 
   // Check if the URL path is more than just /path/ and starts with a language code
   let path_parts = base_pathname.split("/");
