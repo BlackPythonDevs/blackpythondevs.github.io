49e572e34657968b150ec8cd6314bedd3f1eccdd
Oleksis Fraga Menéndez <oleksis.fraga@gmail.com>
2023-11-22 13:28:32 -0500

    Remove Deprecated Repos from Devcontainer Fixes #137

--- .github/.devcontainer/post-create-bpdevs.sh
+++ .github/.devcontainer/post-create-bpdevs.sh
@@ -9,3 +9,19 @@ BPD_DIR= "$DIR/../.."
 pre-commit install
 py -m pip install --no-cache-dir -r ${BPD_DIR}/requirements-dev.txt
 py -m playwright install --with-deps
+
+# List the vscode-codespaces extensions
+ls ~/.vscode-remote/extensions
+
+# Define the extensions to be deleted
+extensions=("rebornix.ruby" "wingrunr21.vscode-ruby")
+
+for extension in "${extensions[@]}"; do
+  # Check if the extension directory exists
+  if [ -d "~/.vscode-remote/extensions/$extension"* ]; then
+    echo "Deleting $extension"
+    rm -rf ~/.vscode-remote/extensions/$extension-*
+  else
+    echo "$extension does not exist"
+  fi
+done
