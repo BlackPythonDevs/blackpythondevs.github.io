607bae9a6fa7dbd8736b444fed2bbf530ca8b0ab
Oleksis Fraga Menéndez <oleksis.fraga@gmail.com>
2023-11-22 02:56:39 -0500

    Update path to requirements-dev.txt

--- .github/.devcontainer/post-create-bpdevs.sh
+++ .github/.devcontainer/post-create-bpdevs.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 pre-commit install
-py -m pip install --no-cache-dir -r ~/.local/requirements-dev.txt
+py -m pip install --no-cache-dir -r ./requirements-dev.txt
 py -m playwright install --with-deps
