d068b95b4e3239fe8708c31396baff0e52b184a1
Oleksis Fraga Menéndez <oleksis.fraga@gmail.com>
2023-11-22 01:28:57 -0500

    Add Ruby 3.2.2 devcontainer feature

--- .github/.devcontainer/post-create-bpdevs.sh
+++ .github/.devcontainer/post-create-bpdevs.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 pre-commit install
-py -m pip install --no-cache-dir -r /tmp/requirements-dev.txt
+py -m pip install --no-cache-dir -r ~/.local/requirements-dev.txt
 py -m playwright install --with-deps
-rbenv install
