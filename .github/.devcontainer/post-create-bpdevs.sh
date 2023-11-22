507eb151aa6170145f216ecaef1d862f16680c8b
Oleksis Fraga Menéndez <oleksis.fraga@gmail.com>
2023-11-22 03:20:32 -0500

    Update post create script

--- .github/.devcontainer/post-create-bpdevs.sh
+++ .github/.devcontainer/post-create-bpdevs.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# Get the directory of the current script
+DIR=$(dirname "$0")
+
+# Navigate to the root directory
+BPD_DIR= "$DIR/../.."
+
 pre-commit install
-py -m pip install --no-cache-dir -r ./requirements-dev.txt
+py -m pip install --no-cache-dir -r ${BPD_DIR}/requirements-dev.txt
 py -m playwright install --with-deps
