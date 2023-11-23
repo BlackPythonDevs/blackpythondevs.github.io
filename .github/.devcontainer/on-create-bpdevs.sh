0aae65516f56e8a0156e643052f09ca06bc90701
Oleksis Fraga Menéndez <44526468+oleksis@users.noreply.github.com>
2023-11-23 16:17:09 +0000

    Add include dirs on create script

--- .github/.devcontainer/on-create-bpdevs.sh
+++ .github/.devcontainer/on-create-bpdevs.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-rbenv install
+rbenv install $(cat .ruby-version)
+rbenv global $(cat .ruby-version)
+
+PRISM_DIR=/usr/local/rvm/gems/default/gems/prism-0.17.1
+export C_INCLUDE_PATH=$PRISM_DIR/include:$PRISM_DIR/ext:$C_INCLUDE_PATH
