d210966e9e9beca70531fb34aeddda883514526a
Oleksis Fraga Menéndez <44526468+oleksis@users.noreply.github.com>
2023-11-23 17:18:58 +0000

    Update Gemfile and on create script

--- .github/.devcontainer/on-create-bpdevs.sh
+++ .github/.devcontainer/on-create-bpdevs.sh
@@ -3,5 +3,5 @@
 rbenv install $(cat .ruby-version)
 rbenv global $(cat .ruby-version)
 
-PRISM_DIR=/usr/local/rvm/gems/default/gems/prism-0.17.1
-export C_INCLUDE_PATH=$PRISM_DIR/include:$PRISM_DIR/ext:$C_INCLUDE_PATH
+# PRISM_DIR=/usr/local/rvm/gems/default/gems/prism-0.17.1
+# export C_INCLUDE_PATH=$PRISM_DIR/include:$PRISM_DIR/ext:$C_INCLUDE_PATH
