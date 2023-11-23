338203e94b19a1016cb57a4c2b3a7bffbf362209
Oleksis Fraga Menéndez <44526468+oleksis@users.noreply.github.com>
2023-11-23 03:31:08 +0000

    Update post create script Install build essential tools

--- .github/.devcontainer/on-create-bpdevs.sh
+++ .github/.devcontainer/on-create-bpdevs.sh
@@ -1,22 +1,3 @@
 #!/bin/sh
 
 rbenv install
-bundle config build.prism --enable-static
-
-# List the vscode-codespaces extensions
-ls ~/.vscode-remote/extensions
-
-# Define the extensions to be deleted
-extension1="rebornix.ruby"
-extension2="wingrunr21.vscode-ruby"
-
-# Check if the extension directories exist and delete them
-for extension in $extension1 $extension2
-do
-  if [ -d ~/.vscode-remote/extensions/$extension-* ]; then
-    echo "Deleting $extension"
-    rm -rf ~/.vscode-remote/extensions/$extension-*
-  else
-    echo "$extension does not exist"
-  fi
-done
