215ae5329619e4ecbeb60186fda21d88df3702d4
Oleksis Fraga Menéndez <44526468+oleksis@users.noreply.github.com>
2023-11-24 01:52:58 +0000

    Update BPDevs vscode Profile Update Dockerfile

--- .github/.devcontainer/on-create-bpdevs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-rbenv install $(cat .ruby-version)
-rbenv global $(cat .ruby-version)
-
-# PRISM_DIR=/usr/local/rvm/gems/default/gems/prism-0.17.1
-# export C_INCLUDE_PATH=$PRISM_DIR/include:$PRISM_DIR/ext:$C_INCLUDE_PATH
