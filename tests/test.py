56c95fcf8320348089c41323859a16f25afd15e8
Alex Oladele <dragid10@gmail.com>
2024-08-30 17:50:50 -0400

    Add tests for parsing Conference Issues

--- tests/test.py
+++ tests/test.py
@@ -1,6 +1,8 @@
 import pytest
 from playwright.sync_api import Page, expect
 
+from _conferences.__main__ import parse_conference_details
+
 live_server_url = "http://127.0.0.1:4000"
 
 routes = [
@@ -13,8 +15,8 @@ routes = [
 
 @pytest.mark.parametrize("url", routes)
 def test_destination(
-    page: Page,
-    url: str,
+        page: Page,
+        url: str,
 ) -> None:
     """Test that the destinations page loads with seeded data"""
     # Create a destination
@@ -26,11 +28,11 @@ def test_destination(
 @pytest.mark.parametrize(
     "title, url",
     (
-        ("Acerca de", "/es/about/"),
-        ("Inicio", "/es/"),
-        ("Eventos", "/es/events/"),
-        ("Comunidad", "/es/community/"),
-        ("Conferencias", "/es/conferences/"),
+            ("Acerca de", "/es/about/"),
+            ("Inicio", "/es/"),
+            ("Eventos", "/es/events/"),
+            ("Comunidad", "/es/community/"),
+            ("Conferencias", "/es/conferences/"),
     ),
 )
 def test_headers_in_language(page: Page, title: str, url: str) -> None:
@@ -55,11 +57,11 @@ def test_switching_lang_es_about(page: Page) -> None:
 @pytest.mark.parametrize(
     "title, url",
     (
-        ("Kutuhusu", "/sw/about/"),
-        ("Nyumbani", "/sw/"),
-        ("Matukio", "/sw/events/"),
-        ("Jumuiya", "/sw/community/"),
-        ("Mikutano", "/sw/conferences/"),
+            ("Kutuhusu", "/sw/about/"),
+            ("Nyumbani", "/sw/"),
+            ("Matukio", "/sw/events/"),
+            ("Jumuiya", "/sw/community/"),
+            ("Mikutano", "/sw/conferences/"),
     ),
 )
 def test_headers_in_sw(page: Page, title: str, url: str) -> None:
@@ -84,12 +86,12 @@ def test_switching_lang_sw_about(page: Page) -> None:
 @pytest.mark.parametrize(
     "title, url",
     (
-        ("Black Python Devs | Home", "/"),
-        ("Black Python Devs | Blog", "/blog"),
-        ("Black Python Devs | About Us", "/about/"),
-        ("Black Python Devs | Events", "/events/"),
-        ("Black Python Devs | Conferences", "/conferences/"),
-        ("Black Python Devs | Community", "/community/"),
+            ("Black Python Devs | Home", "/"),
+            ("Black Python Devs | Blog", "/blog"),
+            ("Black Python Devs | About Us", "/about/"),
+            ("Black Python Devs | Events", "/events/"),
+            ("Black Python Devs | Conferences", "/conferences/"),
+            ("Black Python Devs | Community", "/community/"),
     ),
 )
 def test_bpdevs_title_en(page: Page, title: str, url: str) -> None:
@@ -101,3 +103,113 @@ def test_mailto_bpdevs(page: Page) -> None:
     page.goto(f"{live_server_url}")
     mailto = page.get_by_role("link", name="email")
     expect(mailto).to_have_attribute("href", "mailto:contact@blackpythondevs.com")
+
+
+def test_conference_parsing_valid_url():
+    example_conf_issue = """### Conference Name
+
+Test Conference Title
+
+### URL
+
+https://microsoft.com
+
+### Conference Dates
+
+10 - 15 Sep 2050
+
+### Conference Type
+
+both
+
+### Conference Location
+
+Redmond, WA, USA
+
+### Summary
+
+Test Conference Summary
+
+### Speaking
+
+* [Satya Nadella](https://www.linkedin.com/in/satyanadella/)
+"""
+    expected_name = "Test Conference Title"
+    expected_url = "https://microsoft.com"
+    parsed_conf = parse_conference_details(issue_body=example_conf_issue)
+
+    assert parsed_conf["name"] == expected_name
+    assert parsed_conf["url"] == expected_url
+
+
+def test_conference_parsing_logic_no_url_scheme():
+    example_conf_issue = """### Conference Name
+
+Test Conference Title
+
+### URL
+
+microsoft.com
+
+### Conference Dates
+
+10 - 15 Sep 2050
+
+### Conference Type
+
+both
+
+### Conference Location
+
+Redmond, WA, USA
+
+### Summary
+
+Test Conference Summary
+
+### Speaking
+
+* [Satya Nadella](https://www.linkedin.com/in/satyanadella/)
+"""
+    expected_name = "Test Conference Title"
+    expected_url = "https://microsoft.com"
+    parsed_conf = parse_conference_details(issue_body=example_conf_issue)
+
+    assert parsed_conf["name"] == expected_name
+    assert parsed_conf["url"] == expected_url
+
+
+def test_conference_parsing_logic_no_url():
+    example_conf_issue = """### Conference Name
+
+Test Conference Title
+
+### URL
+
+
+### Conference Dates
+
+10 - 15 Sep 2050
+
+### Conference Type
+
+both
+
+### Conference Location
+
+Redmond, WA, USA
+
+### Summary
+
+Test Conference Summary
+
+### Speaking
+
+* [Satya Nadella](https://www.linkedin.com/in/satyanadella/)
+"""
+    expected_name = "Test Conference Title"
+    expected_url = None
+    parsed_conf = parse_conference_details(issue_body=example_conf_issue)
+
+    assert parsed_conf["name"] == expected_name
+    assert parsed_conf["url"] == expected_url
