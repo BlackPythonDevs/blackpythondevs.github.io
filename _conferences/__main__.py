3985df4d4c83cf846387b6fe6dc28f55dca87ff4
Jay Miller <kjaymiller@gmail.com>
2024-10-04 21:00:53 -0400

    update issue template
    update issue template to include start and end dates for conference
    
    update conferences to use gh-issues instead of manual parsing
    
    update conferences template to use grid and new json methods
    
    add conferences.json
    
    remove envrc

--- _conferences/__main__.py
+++ _conferences/__main__.py
@@ -1,83 +1,19 @@
-import os
-import re
-from datetime import datetime, time
-from pathlib import Path
+import datetime
+import pathlib
 from urllib.parse import urlparse
+from typing import Iterator
 
-import yaml
-from github import Auth, Github
-from github.Issue import Issue
-from github.PaginatedList import PaginatedList
-
-ROOT = Path(__file__).parent.parent
-conferences_path = ROOT / "_data/conferences.yml"
+import json
+import gh_issues
 
 
-def create_github_client():
-    gh_token = os.getenv("GITHUB_TOKEN", "")
-    auth = Auth.Token(gh_token)
-    client = Github(auth=auth)
-    return client
-
-
-def get_open_issues(gh: Github) -> PaginatedList[Issue]:
-    repo = gh.get_repo("BlackPythonDevs/blackpythondevs.github.io")
-    issues = repo.get_issues(state="open", labels=["conference"])
+def get_conference_issues() -> Iterator[gh_issues.Issue]:
+    query = "repo:blackpythondevs/blackpythondevs.github.io type:issue label:conference"
+    issues = gh_issues.issues_by_query(query)
     return issues
 
 
-def parse_conference_details(issue_body: str) -> dict | None:
-    # Extract fields from issue body
-    name_match = re.search(
-        r"Conference Name(?:\r\n|\n){2}(.*?)(?:\r\n|\n){2}", issue_body
-    )
-    url_match = re.search(r"URL(?:\r\n|\n){2}(.*?)(?:\r\n|\n){2}", issue_body)
-    dates_match = re.search(
-        r"Conference Dates(?:\r\n|\n){2}(.*?)(?:\r\n|\n){2}", issue_body
-    )
-    type_match = re.search(
-        r"Conference Type(?:\r\n|\n){2}(.*?)(?:\r\n|\n){2}", issue_body
-    )
-    location_match = re.search(
-        r"Conference Location(?:\r\n|\n){2}(.*?)(?:\r\n|\n){2}", issue_body
-    )
-    summary_match = re.search(
-        r"Summary(?:\r\n|\n){2}(.*?)(?:\r\n|\n){2}",
-        issue_body,
-        re.DOTALL,
-    )
-    speaking_match = re.search(
-        r"Speaking(?:\r\n|\n){2}(.*?)(?:\r\n|\n){2}### Code of Conduct(?:\r\n|\n){2}",
-        issue_body,
-        re.DOTALL,
-    )
-
-    # Set a default value of None for when the url field isn't as expected
-    valid_url = normalize_url() if not url_match else normalize_url(url_match[1])
-
-    if dates_match:
-        conferenceDates = dates_match[1]
-        # Parse the end date of the conference
-        endDateStr = conferenceDates.split("-")[1].strip()
-        endDate = datetime.strptime(endDateStr, "%d %b %Y")
-        # Check if the conference end date is greater than today
-        today = datetime.combine(datetime.now(), time())
-
-        if endDate >= today:
-            conference = {
-                "name": name_match[1],
-                "url": valid_url,
-                "dates": dates_match[1],
-                "type": type_match[1],
-                "location": location_match[1],
-                "summary": summary_match[1],
-                "speaking": speaking_match[1] if speaking_match else "",
-            }
-            return conference
-    return None
-
-
-def normalize_url(url_match: str = None):
+def normalize_url(url_match: str = str):
     valid_url = None
     # Ensure the url field is not blank and the url matches the regex
     if url_match is not None and url_match.strip() != "":
@@ -85,33 +21,54 @@ def normalize_url(url_match: str = None):
         # If not, then prepend `https` to the url from the issue body
         # This guards against the website thinking the passed in url is another page on https://blackpythondevs.com/
         parsed_url = urlparse(url_match)
+
         if "http" not in parsed_url.scheme.casefold():
             valid_url = f"https://{url_match}"
         else:
             valid_url = url_match
+
     return valid_url
 
 
 def write_conferences_to_file(confs: list[dict]):
     # Write the conferences to the _data/conferences.yml file
-    with conferences_path.open("w") as f:
-        yaml.dump(confs, f)
+    conferences_path.write_text(json.dumps(confs))
+
+
+def __to_conference_date(conference_date: str) -> datetime.date:
+    return datetime.date.fromisoformat(conference_date)
 
 
 if __name__ == "__main__":
+    KEYS = [
+        "conference_name",
+        "url",
+        "conference_start_date",
+        "conference_end_date",
+        "conference_type",
+        "conference_location",
+        "summary",
+        "speaking",
+    ]
+
     conferences = []
+    for _issue in get_conference_issues():
+        if not hasattr(_issue, "conference_end_date"):
+            continue
 
-    # Create Github client object
-    gh_client = create_github_client()
+        if __to_conference_date(_issue.conference_end_date) >= datetime.date.today():
+            conferences.append({k: getattr(_issue, k, None) for k in KEYS})
 
-    # Get open issues from repo
-    open_issues: PaginatedList[Issue] = get_open_issues(gh_client)
-
-    # Parse each conference issue so long as it has the "conference" label
-    for issue in open_issues:
-        if "conference" in [label.name for label in issue.labels]:
-            parsed_conf = parse_conference_details(issue_body=issue.body)
-            if parsed_conf:
-                conferences.append(parsed_conf)
-
-    write_conferences_to_file(conferences)
+    ROOT = pathlib.Path(__file__).parent.parent
+    conferences_path = ROOT.joinpath("_data/conferences.json")
+    conferences_path.write_text(
+        json.dumps(
+            list(
+                sorted(
+                    conferences,
+                    key=lambda x: __to_conference_date(x["conference_start_date"]),
+                )
+            ),
+            indent=2,
+        )
+    )
---
+++ support.md
@@ -0,0 +1,15 @@
+---
+layout: default
+lang: en
+title: Supporting Black Python Devs
+---
+
+There are a lot of ways that you can support the Black Python Devs community.
+
+## Supporting Financially
+
+Black Python devs uses donations to support local user groups and conferences in communities of Black developers, hosting local Black Python Devs events, and covering our operation costs. You can review our [pitch deck](https://www.canva.com/design/DAGKXBERZ2s/J7Z-F5R2j3bw8Unj4SAeZw/edit?utm_content=DAGKXBERZ2s&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton) for a detailed view of the efforts and initiatives we support.
+
+Black Python Devs is a 501c3 under the GNOME Foundation. You can make a tax deductable donation using our CommitChange widget below.
+
+{%- include support.html -%}
