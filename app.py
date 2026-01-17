0dcbd043ff6d976225edf0914121956e4d984581
Jay Miller <kjaymiller@gmail.com>
2026-01-17 15:48:58 -0500

    Refactor navigation menu: remove Home/Community, rename Blog, combine Events and Support

--- app.py
+++ app.py
@@ -6,17 +6,9 @@ from render_engine import Site, Page, Collection, Blog
 from render_engine_markdown import MarkdownPageParser
 
 navigation = [
-    {"text": "Home", "url": "/index.html", "fa": "fa fa-home fa-fw"},
-    {"text": "Blog", "url": "/blog/blog1.html", "fa": "fa fa-newspaper fa-fw"},
+    {"text": "News", "url": "/blog/blog1.html", "fa": "fa fa-newspaper fa-fw"},
     {"text": "About Us", "url": "/about.html", "fa": "fa fa-info-circle fa-fw"},
-    {"text": "BPD Events", "url": "/bpd-events", "fa": "fa fa-calendar fa-fw"},
-    {
-        "text": "Sponsored Events",
-        "url": "/sponsored-events.html",
-        "fa": "fa fa-handshake fa-fw",
-    },
-    {"text": "Community", "url": "/community.html", "fa": "fa fa-users fa-fw"},
-    {"text": "Discounts", "url": "/partnerships.html", "fa": "fa-regular fa-handshake"},
+    {"text": "Events", "url": "/events.html", "fa": "fa fa-calendar fa-fw"},
     {
         "text": "Support Us",
         "url": "/support.html",
@@ -54,6 +46,9 @@ class Support(Page):
     content_path = "support.md"
     template = "support.html"
     data = json.loads(pathlib.Path("_data/foundational_supporters.json").read_text())
+    template_vars = {
+        "partnerships": json.loads(pathlib.Path("_data/partnerships.json").read_text())
+    }
 
 
 @app.page
---
+++ pages/events.md
@@ -0,0 +1,17 @@
+---
+layout: default
+lang: en
+title: Events
+---
+
+# Events
+
+Stay connected with what's happening in the Black Python Devs community and beyond.
+
+## [BPD Events](/bpd-events)
+
+Check out our own community events, meetups, and gatherings.
+
+## [Sponsored Events](/sponsored-events.html)
+
+Discover events from our sponsors and partners that support our community.
