e986b0293af05c3bf9e0f26abe32d46532ec2ad6
Jay Miller <kjaymiller@gmail.com>
2026-03-23 00:01:21 -0400

    removes playwright to test against output (#860)
    * removes playwright to test against output
    
    * reformate
    
    * apply changes

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,4 @@
 import pytest
-import ephemeral_port_reserve
-
-
-@pytest.fixture(scope="module")
-def url_port() -> tuple[str, int]:
-    """returns the localhost and port"""
-    hostname = ephemeral_port_reserve.LOCALHOST
-    free_port = ephemeral_port_reserve.reserve(hostname)
-    url = f"http://{hostname}:{free_port}"
-    return url, free_port
-
 
 ROUTES = [
     "",
@@ -22,17 +11,4 @@ ROUTES = [
 
 @pytest.fixture(params=ROUTES)
 def loaded_route(request):
-    return request.param
-
-
-PROFILES = [
-    "iPhone 15",
-    "iPhone 15 landscape",
-    "iPhone 15 Plus",
-    "iPhone 15 Plus landscape",
-]
-
-
-@pytest.fixture(scope="session", params=PROFILES)
-def loaded_profile(request):
     return request.param
